395f18685a7e3bfb312ae365759ee3fb84cb459f
auridevil <onailerua@hotmail.com>
2016-09-30 11:55:39 +0200

    dependencies update

--- test/lib/log.js
+++ test/lib/log.js
@@ -1,71 +1,71 @@
-var util   = require("gulp-util");
-var should = require("should-promised");
-var sinon  = require("sinon");
+var util = require("gulp-util");
+var should = require("should");
+var sinon = require("sinon");
 
 var log = require("lib/log.js");
 
-describe("The `log.start` function", function () {
+describe("The `log.start` function", function() {
 
-    beforeEach(function () {
-        sinon.stub(util, "log");
-    });
+beforeEach(function() {
+    sinon.stub(util, "log");
+});
 
-    afterEach(function () {
-        util.log.restore();
-    });
+afterEach(function() {
+    util.log.restore();
+});
 
-    it("should attach a Date object to the property `start` of its context", function () {
-        var ctx = {};
-        log.start.call(ctx);
-        ctx.start.should.be.instanceOf(Date);
-    });
+it("should attach a Date object to the property `start` of its context", function() {
+    var ctx = {};
+    log.start.call(ctx);
+    ctx.start.should.be.instanceOf(Date);
+});
 
-    it("should call the `gulp-util.log` function", function () {
-        log.start.call({});
-        util.log.calledWithMatch(/Starting/).should.equal(true);
-    });
+it("should call the `gulp-util.log` function", function() {
+    log.start.call({});
+    util.log.calledWithMatch(/Starting/).should.equal(true);
+});
 
 });
 
-describe("The `log.end` function", function () {
+describe("The `log.end` function", function() {
 
-    beforeEach(function () {
-        sinon.stub(util, "log");
-    });
+beforeEach(function() {
+    sinon.stub(util, "log");
+});
 
-    afterEach(function () {
-        util.log.restore();
-    });
+afterEach(function() {
+    util.log.restore();
+});
 
-    it("should attach a Date object to the property `end` of its context", function () {
-        var ctx = {
-            start: new Date()
-        };
-        log.end.call(ctx);
-        ctx.end.should.be.instanceOf(Date);
-    });
+it("should attach a Date object to the property `end` of its context", function() {
+    var ctx = {
+        start: new Date()
+    };
+    log.end.call(ctx);
+    ctx.end.should.be.instanceOf(Date);
+});
 
-    it("should call the `gulp-util.log` function", function () {
-        log.end.call({});
-        util.log.calledWithMatch(/Finished/).should.equal(true);
-    });
+it("should call the `gulp-util.log` function", function() {
+    log.end.call({});
+    util.log.calledWithMatch(/Finished/).should.equal(true);
+});
 
 
 });
 
-describe("The `log.error` function", function () {
+describe("The `log.error` function", function() {
 
-    beforeEach(function () {
-        sinon.stub(util, "log");
-    });
+beforeEach(function() {
+    sinon.stub(util, "log");
+});
 
-    afterEach(function () {
-        util.log.restore();
-    });
+afterEach(function() {
+    util.log.restore();
+});
 
-    it("should call the `gulp-util.log` function", function () {
-        log.error.call({});
-        util.log.calledWithMatch(/Terminated/).should.equal(true);
-    });
+it("should call the `gulp-util.log` function", function() {
+    log.error.call({});
+    util.log.calledWithMatch(/Terminated/).should.equal(true);
+});
 
 });
--- test/lib/promisify.js
+++ test/lib/promisify.js
@@ -1,110 +1,112 @@
 var BPromise = require("bluebird");
-var events   = require("events");
-var should   = require("should-promised");
-var sinon    = require("sinon");
+var events = require("events");
+var should = require("should");
+var sinon = require("sinon");
 
 var promisify = require("lib/promisify.js");
 
-describe("The `promisify` function", function () {
+describe("The `promisify` function", function() {
 
-    it("should returned a function", function () {
-        var ret = promisify();
-        ret.should.be.of.type("function");
-    });
+it("should returned a function", function() {
+    var ret = promisify();
+    ret.should.be.of.type("function");
+});
 
 });
 
-describe("The function returned by `promisify`", function () {
+describe("The function returned by `promisify`", function() {
 
-    it("should invoke the function supplied to `promisify`, when called", function () {
-        var spy = sinon.spy();
-        var fn = promisify(spy);
-        fn();
-        spy.called.should.equal(true);
-    });
+it("should invoke the function supplied to `promisify`, when called", function() {
+    var spy = sinon.spy();
+    var fn = promisify(spy);
+    fn();
+    spy.called.should.equal(true);
+});
 
-    it("should return a promise", function () {
-        var spy = sinon.spy();
-        var fn = promisify(spy);
-        var ret = fn();
-        ret.should.be.a.Promise;
-    });
+it("should return a promise", function() {
+    var spy = sinon.spy();
+    var fn = promisify(spy);
+    var ret = fn();
+    ret.should.be.a.Promise;
+});
 
 });
 
-describe("When the supplied function returns a stream, the promise returned by the wrapper", function () {
+describe("When the supplied function returns a stream, the promise returned by the wrapper", function() {
 
-    it("should be fulfilled when the stream finishes", function () {
-        var stream = new events.EventEmitter();
-        stream.pipe = function noop () {};
-        var fn = promisify(function () {
-            return stream;
-        });
-        var ret = fn();
-        stream.emit("finish");
-        return ret.should.be.fulfilled;
+it("should be fulfilled when the stream finishes", function() {
+    var stream = new events.EventEmitter();
+    stream.pipe = function noop() {};
+    var fn = promisify(function() {
+        return stream;
     });
+    var ret = fn();
+    stream.emit("finish");
+    return ret.should.be.fulfilled;
+});
 
-    it("should be fulfilled when the stream ends", function () {
-        var stream = new events.EventEmitter();
-        stream.pipe = function noop () {};
-        var fn = promisify(function () {
-            return stream;
-        });
-        var ret = fn();
-        stream.emit("end");
-        return ret.should.be.fulfilled;
+it("should be fulfilled when the stream ends", function() {
+    var stream = new events.EventEmitter();
+    stream.pipe = function noop() {};
+    var fn = promisify(function() {
+        return stream;
     });
+    var ret = fn();
+    stream.emit("end");
+    return ret.should.be.fulfilled;
+});
 
-    it("should be rejected when the stream errors", function () {
-        var stream = new events.EventEmitter();
-        stream.pipe = function noop () {};
-        var fn = promisify(function () {
-            return stream;
-        });
-        var ret = fn();
-        stream.emit("error");
-        return ret.should.be.rejected;
+it("should be rejected when the stream errors", function() {
+    var stream = new events.EventEmitter();
+    stream.pipe = function noop() {};
+    var fn = promisify(function() {
+        return stream;
     });
+    var ret = fn().catch(err => {
+        /*donothing*/
+    });
+    stream.emit("error");
+    return ret.should.be.rejected;
+});
 
 });
 
-describe("When the supplied function returns a promise, the promise returned by the wrapper", function () {
+describe("When the supplied function returns a promise, the promise returned by the wrapper", function() {
 
-    it("should be the same promise proxied through", function () {
-        var promise = BPromise.resolve();
-        var fn = promisify(function () {
-            return promise;
-        });
-        var ret = fn();
-        ret.should.equal(promise);
+it("should be the same promise proxied through", function() {
+    var promise = BPromise.resolve();
+    var fn = promisify(function() {
+        return promise;
     });
+    var ret = fn();
+    ret.should.equal(promise);
+});
 
 });
 
-describe("When the supplied function returns a value, the promise returned by the wrapper", function () {
+describe("When the supplied function returns a value, the promise returned by the wrapper", function() {
 
-    it("should be a promise wrapper around that value", function () {
-        var value = {};
-        var fn = promisify(function () {
-            return value;
-        });
-        var ret = fn();
-        return ret.should.fulfilledWith(value);
+it("should be a promise wrapper around that value", function() {
+    var value = {};
+    var fn = promisify(function() {
+        return value;
     });
+    var ret = fn();
+    return ret.should.fulfilledWith(value);
+});
 
 });
 
 
-describe("When the supplied function throws, the promise returned by the wrapper", function () {
+describe("When the supplied function throws, the promise returned by the wrapper", function() {
 
-    it("should be a promise that is rejected with the thrown error", function () {
-        var error = new Error();
-        var fn = promisify(function () {
-            throw error;
-        });
-        var ret = fn();
-        ret.should.rejectedWith(error);
+it("should be a promise that is rejected with the thrown error", function() {
+    var error = new Error();
+    var fn = promisify(function() {
+        throw error;
     });
+    var ret = fn();
+    ret.should.rejectedWith(error);
+});
 
 });
